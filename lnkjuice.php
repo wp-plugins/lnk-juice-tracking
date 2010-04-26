987d449abc8d5a4d0d1c427dd9db104354746cce
mliljedahl <mliljedahl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-26 22:21:29 +0000

    tagging version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/lnk-juice-tracking/tags/1.2.1@234183 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lnkjuice.php
+++ lnkjuice.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Lnk Juice Tracking
-Plugin URI: hhttp://www.liljedahl.info/projects/wp-lnkjuice/
+Plugin URI: hhttp://www.liljedahl.me/projekt/wp-lnkjuice/
 Description: Simple plugin that adds your Lnk Juice tracking code to the head tag. When activated you must <a href="plugins.php?page=lnkjuice-config">enter your Lnk Juice Tracking Code</a> under the Plugins menu for it to work. If you do not yet have an account go get one at <a href="http://www.lnkjuice.com/">LnkJuice.com</a>!
 Author: Markus Liljedahl 
-Version: 1.1
-Author URI: http://www.liljedahl.info
+Version: 1.2.1
+Author URI: http://www.liljedahl.me/
 */
 
 // For backwards compatibility, esc_attr_e was added in 2.8 and attribute_escape is from 2.8 marked as deprecated.
@@ -16,13 +16,14 @@ if (! function_exists('esc_attr_e')) {
 }
 
 // The html code that goes in to the header
-function add_LnkJuice_head() {
-	$code = get_option('lnkjuice_tracking_code');
-	if(!is_admin() && strlen($code) == 32) {
+function add_LnkJuice_footer() {
+	$code = get_option('lnkjuice_cached_code');
+	
+	if(!is_admin() && strlen($code) >= 32) {
 		echo '<script type="text/javascript" src="http://stat.lnkjuice.com/lt.js"></script>
 <script type="text/javascript">
 <!--
-   roza_hrefReplace("'.$code.'");
+   roza_hrefReplace('.$code.');
 // -->
 </script>' . PHP_EOL;
 		echo PHP_EOL;
@@ -37,15 +38,33 @@ function print_LnkJuice_management() {
 		
 		$code = trim($_POST['lnkjuice_tracking_code']);
 		
-		if (strlen($code) == 32) {
+		if (strlen($code) > 0) {
+			
+			$identifyer = trim($_POST['lnkjuice_detailed_statistics']);
+			$subdomain = $_POST['lnkjuice_disable_subdomain'];
 	
 			if ( empty($code) ) {
 				delete_option('lnkjuice_tracking_code');
 			} else {
 				update_option('lnkjuice_tracking_code', $code);
 			}
+			
+			if ( empty($identifyer) ) {
+				delete_option('lnkjuice_detailed_statistics');
+			} else {
+				update_option('lnkjuice_detailed_statistics', $identifyer);
+			}
+			
+			if ( empty($subdomain) ) {
+				delete_option('lnkjuice_disable_subdomain');
+			} else {
+				update_option('lnkjuice_disable_subdomain', $subdomain);
+			}
+
+			cache_LnkJuice_code();
+
 ?>
-<div id="message" class="updated fade"><p><strong><?php esc_attr_e('Options saved.') ?></strong></p></div>
+<div id="message" class="updated fade"><p><strong><?php esc_attr_e('Options saved.'); ?></strong></p></div>
 <?php
 		
 		}
@@ -57,18 +76,26 @@ function print_LnkJuice_management() {
 	
 	<p><?php _e('You need to enter your Lnk Juice Tracking Code here to get the plugin to work. If you do not yet have an account go get one at <a href="http://www.lnkjuice.com/">LnkJuice.com</a>!', 'lnkjuice'); ?></p>
 	
-	<p><?php _e('In your <strong>HTML tracking code</strong> look for a string like:', 'lnkjuice'); ?></p>
+	<p><?php _e('When you login at Lnk Juice go to the control panel and then get your <strong>Tracking ID</strong> wich is located under <strong>My tracking code</strong>. It should look similar this code:', 'lnkjuice'); ?></p>
 	
-	<p><code>roza_hrefReplace("abcdefghijklmnopqrstuvwxyz123456");</code></p>
-	
-	<p><?php _e('Then <strong>Your Tracking Code</strong> is: <em>abcdefghijklmnopqrstuvwxyz123456</em>', 'lnkjuice'); ?></p>
-	
+	<p><code>abcdefghijklmnopqrstuvwxyz123456</code></p>
+		
 	<form method="post" action="">
 	
 		<table class="form-table">
 			<tr valign="top">
-			<th scope="row"><label for="lnkjuice_tracking_code"><?php esc_attr_e('Your Tracking Code', 'lnkjuice'); ?></label></th>
-			<td><input name="lnkjuice_tracking_code" type="text" id="lnkjuice_tracking_code" value="<?php echo get_option('lnkjuice_tracking_code'); ?>" class="regular-text" maxlength="32" /></td>
+			<th scope="row"><label for="lnkjuice_tracking_code"><?php esc_attr_e('Your Tracking ID', 'lnkjuice'); ?></label></th>
+			<td><input name="lnkjuice_tracking_code" type="text" id="lnkjuice_tracking_code" value="<?php echo get_option('lnkjuice_tracking_code'); ?>" class="regular-text" maxlength="32" /><br /><span class="description"><?php esc_attr_e('Leave empty to remove', 'lnkjuice'); ?></span></td>
+			<tr>
+
+			<tr valign="top">
+			<th scope="row"><label for=""><?php esc_attr_e('Detailed statistics identifier', 'lnkjuice'); ?></label></th>
+			<td><input name="lnkjuice_detailed_statistics" type="text" id="lnkjuice_detailed_statistics" value="<?php echo get_option('lnkjuice_detailed_statistics'); ?>" class="regular-text" maxlength="100" /><br /><span class="description"><?php esc_attr_e('Use up to 100 characters, (a-z, 0-9 and .)', 'lnkjuice'); ?></span></td>
+			</tr>
+
+			<tr valign="top">
+			<th scope="row"><label for=""><?php esc_attr_e('Disable subdomain conversion', 'lnkjuice'); ?></label></th>
+			<td><input name="lnkjuice_disable_subdomain" type="checkbox" id="lnkjuice_disable_subdomain" value="false" <?php if ( get_option('lnkjuice_disable_subdomain') ) echo "checked=\"checked\""?>></td>
 			</tr>
 		</table>
 		
@@ -82,6 +109,26 @@ function print_LnkJuice_management() {
 <?php
 }
 
+function cache_LnkJuice_code()
+{
+	$code = get_option('lnkjuice_tracking_code');
+	$identifyer = get_option('lnkjuice_detailed_statistics');
+	$subdomain = get_option('lnkjuice_disable_subdomain');
+
+	if ($code && $identifyer && $subdomain) {
+		$fullCode = '"'.$code.'+'.$identifyer.'", '.$subdomain;
+		update_option('lnkjuice_cached_code', $fullCode);
+	} elseif ($code && $identifyer) {
+		$fullCode = '"'.$code.'+'.$identifyer.'"';
+		update_option('lnkjuice_cached_code', $fullCode);
+	} elseif ($code) {
+		$fullCode = '"'.$code.'"';
+		update_option('lnkjuice_cached_code', $fullCode);
+	} else {
+		delete_option('lnkjuice_cached_code');
+	}
+}
+
 function add_LnkJuice_admin_page() 
 {
 	if ( function_exists('add_submenu_page') )
@@ -89,6 +136,6 @@ function add_LnkJuice_admin_page()
 }
 
 load_plugin_textdomain('lnkjuice', 'wp-content/plugins/' . dirname(plugin_basename(__FILE__)) . '/i18n');
-add_action('wp_head', 'add_LnkJuice_head');
+add_action('wp_footer', 'add_LnkJuice_footer');
 add_action('admin_menu', 'add_LnkJuice_admin_page');
 ?>
